3483c41e36043406ece174b6be1f3f1f47cf9e7b
Kevin <hberg539@gmail.com>
2011-07-08 07:47:23 +0200

    added set switch for rdate crontab entry

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ apt-get update && apt-get install vim screen vnstat rdate ssh less
 # add rdate crontab
 cat >> /etc/crontab <<EOF
 # rdate
-0 0     * * *   root    /usr/bin/rdate time.fu-berlin.de > /dev/null 2> /var/log/rdate.log
+0 0     * * *   root    /usr/bin/rdate -s time.fu-berlin.de
 EOF
 
